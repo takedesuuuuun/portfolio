b7bd0647ffb182c3980fea5246c986f1441f15f3
takedesuuuuun <takedesuuuuun@gmail.com>
2026-06-29 14:43:15 +0900

    meta tag

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Header } from "../components/header"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Portfolio" },
+    { name: "description", content: "takedesuuuuun's portfolio." },
   ];
 }
 
