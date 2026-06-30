10b1e648f80910bc5aa8adfd5eaa4b8751533d54
takedesuuuuun <takedesuuuuun@gmail.com>
2026-06-30 15:44:18 +0900

    hamburgerMenu v1

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import "./header.css"
 
 export function Header() {
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return(
     <header>
       <div>
-        <h1>ポートフォリオ</h1>
+        <nav>
+          <button
+            className="hamburger"
+            onClick={() => setIsOpen(!isOpen)}
+            >
+            {!isOpen && (
+              <div className="closeHamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            )}
+            {isOpen && (
+              <div className="openHamburger">
+                <div className="leftCross"></div>
+                <div className="rightCross"></div>
+              </div>
+            )}
+          </button>
+          {isOpen && (
+            <div className="menu">
+              <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#works">Works</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          )}
+        </nav>
       </div>
     </header>
   );
