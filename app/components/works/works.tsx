71e0f7eaad2cfa40c9c6b1170e59e85ec1817d34
takedesuuuuun <takedesuuuuun@gmail.com>
2026-07-09 15:32:34 +0900

    works v2

--- app/components/works/works.tsx
+++ app/components/works/works.tsx
@@ -5,11 +5,47 @@ export function Works() {
   type Work = {
     id: number;
     title: string;
+    image: string;
+    link: string;
     description: string;
   };
 
   const works: Work[] = [
-    // No contents
+    {
+      id: 1,
+      title: "Portfolio",
+      image: "../public/Icon.png",
+      link: "#",  //Vercel公開でそのリンク
+      description: "takedesuuuuunのポートフォリオです。"
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      image: "../public/Icon.png",
+      link: "#",  //Vercel公開でそのリンク
+      description: "takedesuuuuunのポートフォリオです。2"
+    },
+    {
+      id: 3,
+      title: "Portfolio",
+      image: "../public/Icon.png",
+      link: "#",  //Vercel公開でそのリンク
+      description: "takedesuuuuunのポートフォリオです。3"
+    },
+    {
+      id: 4,
+      title: "Portfolio",
+      image: "../public/Icon.png",
+      link: "#",  //Vercel公開でそのリンク
+      description: "takedesuuuuunのポートフォリオです。4"
+    },
+    {
+      id: 5,
+      title: "Portfolio",
+      image: "../public/Icon.png",
+      link: "#",  //Vercel公開でそのリンク
+      description: "takedesuuuuunのポートフォリオです。5"
+    }
   ];
   
 
@@ -27,9 +63,16 @@ export function Works() {
             </p>
           </div>
         ) : (
-          <ul>
+          <ul id="contents">
             {works.map((work) => (
-              <li key={work.id}>{work.title}</li>
+              <li key={work.id}>
+                <a href={work.link} target="_blank" rel="noopener noreferrer">
+                  <img src={work.image} alt={work.title} />
+                </a>
+
+                <h4>{work.title}</h4>
+                <p>{work.description}</p>
+              </li>
             ))}
           </ul>
         )}
