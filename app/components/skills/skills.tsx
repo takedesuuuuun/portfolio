4176ae162d012b20739c12bcdbd018f7a1361083
takedesuuuuun <takedesuuuuun@gmail.com>
2026-07-10 13:36:23 +0900

    skills ver.tsx

--- app/components/skills/skills.tsx
+++ app/components/skills/skills.tsx
@@ -2,6 +2,27 @@ import "./skills.css"
 
 export function Skills() {
   
+  type Skill = {
+    name: string;
+    level: string;
+  }
+
+  const skills: Skill[] = [
+    { name: "HTML", level: "prof" },
+    { name: "CSS", level: "prof" },
+
+    { name: "JavaScript", level: "prac" },
+    { name: "Git, Github", level: "prac" },
+    { name: "React, React Router", level: "prac" },
+
+    { name: "Node.js", level: "lear" },
+    { name: "MySQL", level: "lear" },
+    { name: "Ruby", level: "lear" },
+
+    { name: "C# (Unity)", level: "next" },
+    { name: "C++ (Unreal Engine)", level: "next" }
+  ]
+
   return(
     <div id="skills" className="anchor">
       <h2>Skills <small>技術</small></h2>
@@ -12,34 +33,44 @@ export function Skills() {
       <span className="orangeLine"></span>
       <p>十分に扱え、実務で実践していきたいと言えるものです。</p>
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
+        {skills
+          .filter((skill) => skill.level === "prof")
+          .map((skill) => (
+          <li key={skill.name}>{skill.name}</li>
+        ))}
       </ul>
 
       <h3>Practicing <small>練習中・自信 : 中</small></h3>
       <span className="orangeLine"></span>
       <p>ある程度理解しており、使えるように練習しているものです。</p>
       <ul>
-        <li>JavaScript</li>
-        <li>Git, Github</li>
-        <li>React, React Router</li>
+        {skills
+          .filter((skill) => skill.level === "prac")
+          .map((skill) => (
+            <li key={skill.name}>{skill.name}</li>
+          ))}
       </ul>
 
       <h3>Learning <small>学習中・自信 : 小</small></h3>
       <span className="orangeLine"></span>
       <p>学習をしたことがあるが、扱えるとは言えないものです。</p>
       <ul>
-        <li>Node.js</li>
-        <li>MySQL</li>
-        <li>Ruby</li>
+        {skills
+          .filter((skill) => skill.level === "lear")
+          .map((skill) => (
+            <li key={skill.name}>{skill.name}</li>
+          ))}
       </ul>
 
       <h3>Next to Learn <small>今後学習予定</small></h3>
       <span className="orangeLine"></span>
       <p>学習したことはありませんが、将来学習したいもの・興味があるものです。</p>
       <ul>
-        <li>C# (Unity)</li>
-        <li>C++ (Unreal Engine)</li>
+        {skills
+          .filter((skill) => skill.level === "next")
+          .map((skill) => (
+            <li key={skill.name}>{skill.name}</li>
+          ))}
       </ul>
     </div>
   );
