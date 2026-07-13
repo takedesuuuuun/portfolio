19b614afc8535bb8eb480ab2b2d15a800631017b
takedesuuuuun <takedesuuuuun@gmail.com>
2026-07-13 13:20:05 +0900

    footer v1, site v1

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import "./footer.css"
+
+export function Footer() {
+  
+  return(
+    <div id="footer">
+      <footer>
+        <p>takedesuuuuun</p>
+        <p><small>&copy; 2026 takedesuuuuun</small></p>
+      </footer>
+    </div>
+  );
+}
